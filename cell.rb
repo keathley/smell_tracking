7790b8391bbf799a770855d2469f237c601b3349
Chris Keathley <spyc3r@gmail.com>
2014-07-17 23:59:23 -0400

    Hide a few more methods

--- cell.rb
+++ cell.rb
@@ -15,16 +15,6 @@ class Cell
     @p = obj.p
   end
 
-  def x1; x - SIZE/2; end
-  def x2; x + SIZE/2; end
-  def y1; y - SIZE/2; end
-  def y2; y + SIZE/2; end
-
-  def color
-    p = p_to_color_range(@p)
-    Gosu::Color.new(255, p, p, p)
-  end
-
   def x
     @x_index * SIZE + (SIZE/2)
   end
@@ -37,5 +27,15 @@ class Cell
 
   def p_to_color_range(p)
     (((p - 1) * (255 - 0)) / (1000 - 1)) + 0
+  end
+
+  def x1; x - SIZE/2; end
+  def x2; x + SIZE/2; end
+  def y1; y - SIZE/2; end
+  def y2; y + SIZE/2; end
+
+  def color
+    p = p_to_color_range(@p)
+    Gosu::Color.new(255, p, p, p)
   end
 end
